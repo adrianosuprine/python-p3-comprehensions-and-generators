34694fe7179ae8dd411a0946e1ee84b83ff682c2
adrianosuprine <adrianosuprine@gmail.com>
2024-03-01 14:20:39 +0300

    python list comprehensions ans generators

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,16 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    # num_list = range(10)
+    evens_list = [num for num in num_list if num % 2 == 0]
+    return evens_list
+
+return_evens( num_list = range(10))
 
 def make_exclamation(sentence_list):
-    pass
+    exclamation_list = [sentence + "!" for sentence in sentence_list ]
+    return exclamation_list
+    
+
+make_exclamation(["Hello", "I'm doing great", "Python is fun"])
+    
